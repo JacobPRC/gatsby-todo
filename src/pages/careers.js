f2f42a3f3ec30250843f3649a1e0dd4759e267ae
jacob <jacobcunningham96@yahoo.com>
2020-12-01 13:39:40 -0700

    added content to empty pages to avoid errors

---
+++ src/pages/careers.js
@@ -0,0 +1,10 @@
+import { Link } from "gatsby"
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+
+export default () => (
+  <Layout>
+    <h1>Hello World</h1>
+  </Layout>
+)
---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+
+export default () => (
+  <Layout>
+    <h1>Hello World</h1>
+  </Layout>
+)
