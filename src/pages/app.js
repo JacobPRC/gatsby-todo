0a9573a70508ddc52b5cce3fecd5e1796624e322
jacob <jacobcunningham96@yahoo.com>
2020-12-29 19:17:55 -0700

    think i figured out error, pushing to see if all goes well

--- src/pages/app.js
+++ src/pages/app.js
@@ -7,9 +7,12 @@ import Layout from "../components/Layout/Layout"
 export default () => {
   const { user } = useContext(IdentityContext)
   console.log(user)
+
+  if (!user) return <div>Loading...</div>
+
   return (
     <Layout>
-      <h1>Hello world</h1>
+      <h1>Hello {user.user_metadata.full_name}</h1>
     </Layout>
   )
 }
