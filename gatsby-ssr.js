ee36f577a6b2ec1296461f9bd39c3d095ad44041
jacob <jacobcunningham96@yahoo.com>
2021-01-04 15:34:37 -0700

    fixed typo

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,8 @@ import { ApolloProvider } from "react-apollo"
 import { renderToString } from "react-dom/server"
 import fetch from "isomorphic-fetch"
 
+import { Provider } from "./identity-context"
+
 // gatsby-ssr is required for build regardless if you plan to support SSR
 export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
   const client = new ApolloClient({
