699479791dc2064dd75df5a4c09aa90c45c88e26
jacob <jacobcunningham96@yahoo.com>
2021-01-08 21:35:51 -0700

    debug

--- functions/graphql.js
+++ functions/graphql.js
@@ -34,7 +34,7 @@ const resolvers = {
       //need to find out what is being returned from results. This may be the err
       //have to try 2 redeploy this see what comes out from it. netlify is down
       return results.data.map(item => {
-        return console.log(results.data)
+        return results.data.map(i => console.log(i))
         return { done: item.done, text: item.text }
       })
     },
