5d66b3c256024a7844f0202b1297359fcdb19739
jacob <jacobcunningham96@yahoo.com>
2021-01-09 17:06:13 -0700

    debug

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,47 +1,48 @@
 const { ApolloServer, gql } = require("apollo-server-lambda")
 const faunadb = require("faunadb")
-
-const keys = require("../keys")
-
 const q = faunadb.query
 
-const client = new faunadb.Client({ secret: keys.FAUNA || process.env.FAUNA })
+var client = new faunadb.Client({ secret: process.env.FAUNA })
 
+// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    todos(user: ID!): [Todo]!
+    todos: [Todo]!
   }
   type Todo {
-    id: ID
-    text: String
-    done: Boolean
+    id: ID!
+    text: String!
+    done: Boolean!
   }
   type Mutation {
-    addTodo(text: String!, user: ID!): Todo
+    addTodo(text: String!): Todo
     updateTodoDone(id: ID!): Todo
   }
 `
 
+// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    todos: async (parent, { user }) => {
+    todos: async (parent, args, { user }) => {
       if (!user) {
         return []
+      } else {
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index("todos_by_user"), user))
+        )
+        return results.data.map(([ref, text, done]) => ({
+          id: ref.id,
+          text,
+          done,
+        }))
       }
-      const results = await client.query(
-        q.Paginate(q.Match(q.Index("todos_by_user"), user))
-      )
-      //need to find out what is being returned from results. This may be the err
-      //have to try 2 redeploy this see what comes out from it. netlify is down
-      return results.data.map(item => {
-        const obj = { ...item }
-        console.log(obj)
-        return obj
-      })
     },
   },
   Mutation: {
-    addTodo: async (_, { text, user }) => {
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to insert todos")
+      }
       const results = await client.query(
         q.Create(q.Collection("todos"), {
           data: {
@@ -51,13 +52,15 @@ const resolvers = {
           },
         })
       )
-      const balls = {
+      return {
         ...results.data,
         id: results.ref.id,
       }
-      return console.log(balls)
     },
     updateTodoDone: async (_, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to insert todos")
+      }
       const results = await client.query(
         q.Update(q.Ref(q.Collection("todos"), id), {
           data: {
@@ -83,6 +86,11 @@ const server = new ApolloServer({
       return {}
     }
   },
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
   playground: true,
   introspection: true,
 })
@@ -93,3 +101,99 @@ exports.handler = server.createHandler({
     credentials: true,
   },
 })
+
+// const { ApolloServer, gql } = require("apollo-server-lambda")
+// const faunadb = require("faunadb")
+
+// const keys = require("../keys")
+
+// const q = faunadb.query
+
+// const client = new faunadb.Client({ secret: keys.FAUNA || process.env.FAUNA })
+
+// const typeDefs = gql`
+//   type Query {
+//     todos(user: ID!): [Todo]!
+//   }
+//   type Todo {
+//     id: ID
+//     text: String
+//     done: Boolean
+//   }
+//   type Mutation {
+//     addTodo(text: String!, user: ID!): Todo
+//     updateTodoDone(id: ID!): Todo
+//   }
+// `
+
+// const resolvers = {
+//   Query: {
+//     todos: async (parent, { user }) => {
+//       if (!user) {
+//         return []
+//       }
+//       const results = await client.query(
+//         q.Paginate(q.Match(q.Index("todos_by_user"), user))
+//       )
+//       //need to find out what is being returned from results. This may be the err
+//       //have to try 2 redeploy this see what comes out from it. netlify is down
+//       return results.data.map(item => {
+//         const obj = {}
+//         console.log(obj)
+//         return obj
+//       })
+//     },
+//   },
+//   Mutation: {
+//     addTodo: async (_, { text, user }) => {
+//       const results = await client.query(
+//         q.Create(q.Collection("todos"), {
+//           data: {
+//             text,
+//             done: false,
+//             owner: user,
+//           },
+//         })
+//       )
+//       const balls = {
+//         ...results.data,
+//         id: results.ref.id,
+//       }
+//       return console.log(balls)
+//     },
+//     updateTodoDone: async (_, { id }, { user }) => {
+//       const results = await client.query(
+//         q.Update(q.Ref(q.Collection("todos"), id), {
+//           data: {
+//             done: true,
+//           },
+//         })
+//       )
+//       return {
+//         ...results.data,
+//         id: results.ref.id,
+//       }
+//     },
+//   },
+// }
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: ({ context }) => {
+//     if (context.clientContext.user) {
+//       return { user: context.clientContext.user.sub }
+//     } else {
+//       return {}
+//     }
+//   },
+//   playground: true,
+//   introspection: true,
+// })
+
+// exports.handler = server.createHandler({
+//   cors: {
+//     origin: "*",
+//     credentials: true,
+//   },
+// })
