ff4cc4e21cee1f394b112698594e14b2cd4e5c42
jacob <jacobcunningham96@yahoo.com>
2021-01-10 20:45:18 -0700

    debug

--- functions/graphql.js
+++ functions/graphql.js
@@ -34,11 +34,11 @@ const resolvers = {
       console.log(results)
       return results
 
-      const results = await client.query(
-        q.Get(q.Match(q.Index("todos_by_user"), user))
-      )
-      console.log(results)
-      return results
+      // const results = await client.query(
+      //   q.Get(q.Match(q.Index("todos_by_user"), user))
+      // )
+      // console.log(results)
+      // return results
       // const results = await client.query(
       //   q.Paginate(q.Match(q.Index("todos_by_user"), user))
       // )
